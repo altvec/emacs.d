b1356be6767e9c97882c65bdad9b5902261f56a2
altvec <me@sergeykalistratov.com>
2020-12-06 17:22:06 +0500

    set initial frame size

---
+++ early-init.el
@@ -0,0 +1,6 @@
+;; start frame at full height
+(add-to-list 'initial-frame-alist '(height . 120))
+(add-to-list 'initial-frame-alist '(width . 120))
+
+(add-to-list 'default-frame-alist '(height . 120))
+(add-to-list 'default-frame-alist '(width . 120))
